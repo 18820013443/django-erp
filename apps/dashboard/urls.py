d1450dba6a1be491c70b6b8862301b3f94185257
kk <331479961@qq.com>
2022-02-18 00:33:44 +0800

    fix dashboard APIs which displayed as a list

--- apps/dashboard/urls.py
+++ apps/dashboard/urls.py
@@ -3,10 +3,10 @@ from dashboard.views import TotalSummaryView, PreviousSevenDaysRevenueView, Prev
     , PreviousSevenDaysSalesCountView, PreviousHalfYearsMonthsRevenueView, TopTenCustomersView
 
 urlpatterns = [
-    re_path('^total_summary/$', TotalSummaryView.as_view({'get': 'list'})),
-    re_path('^seven_dates_revenue/$', PreviousSevenDaysRevenueView.as_view({'get': 'list'})),
-    re_path('^seven_dates_orders/$', PreviousSevenDaysOrdersCountView.as_view({'get': 'list'})),
-    re_path('^seven_dates_sales/$', PreviousSevenDaysSalesCountView.as_view({'get': 'list'})),
-    re_path('^six_months_revenue/$', PreviousHalfYearsMonthsRevenueView.as_view({'get': 'list'})),
-    re_path('^top_ten_customers/$', TopTenCustomersView.as_view({'get': 'list'}))
+    re_path('^total_summary/$', TotalSummaryView.as_view()),
+    re_path('^seven_dates_revenue/$', PreviousSevenDaysRevenueView.as_view()),
+    re_path('^seven_dates_orders/$', PreviousSevenDaysOrdersCountView.as_view()),
+    re_path('^seven_dates_sales/$', PreviousSevenDaysSalesCountView.as_view()),
+    re_path('^six_months_revenue/$', PreviousHalfYearsMonthsRevenueView.as_view()),
+    re_path('^top_ten_customers/$', TopTenCustomersView.as_view())
 ]
--- apps/dashboard/views.py
+++ apps/dashboard/views.py
@@ -1,7 +1,9 @@
 from dashboard.serializers import TotalSummarySerializer, PreviousSevenDaysRevenueSerializer\
     , PreviousSevenDaysOrdersCountSerializer, PreviousSevenDaysSalesCountSerializer,\
     PreviousHalfYearsMonthsRevenueSerializer, TopTenCustomersSerializer
+from rest_framework.response import Response
 from rest_framework.viewsets import ModelViewSet
+from rest_framework.views import APIView
 
 from sales.models import OrdersHeader
 
@@ -9,31 +11,73 @@ from sales.models import OrdersHeader
 # Create your views here.
 
 
-class TotalSummaryView(ModelViewSet):
-    serializer_class = TotalSummarySerializer
-    queryset = OrdersHeader.objects.all()[:1]
+# class TotalSummaryView(ModelViewSet):
+#     serializer_class = TotalSummarySerializer
+#     queryset = OrdersHeader.objects.all()[:1]
+#
+#     def list(self, request, *args, **kwargs):
+#         qs = OrdersHeader.objects.all().first()
+#         serializer = TotalSummarySerializer(instance=qs)
+#         return Response(serializer.data)
+
+class TotalSummaryView(APIView):
+
+    def get(self, request):
+        qs = OrdersHeader.objects.all().first()
+        serializer = TotalSummarySerializer(instance=qs)
+        return Response(serializer.data)
 
 
-class PreviousSevenDaysRevenueView(ModelViewSet):
-    serializer_class = PreviousSevenDaysRevenueSerializer
-    queryset = OrdersHeader.objects.all()[:1]
+# class PreviousSevenDaysRevenueView(ModelViewSet):
+#     serializer_class = PreviousSevenDaysRevenueSerializer
+#     queryset = OrdersHeader.objects.all()[:1]
+
+class PreviousSevenDaysRevenueView(APIView):
+    def get(self, request):
+        qs = OrdersHeader.objects.all().first()
+        serializer = PreviousSevenDaysRevenueSerializer(instance=qs)
+        return Response(serializer.data)
 
 
-class PreviousSevenDaysOrdersCountView(ModelViewSet):
-    serializer_class = PreviousSevenDaysOrdersCountSerializer
-    queryset = OrdersHeader.objects.all()[:1]
+# class PreviousSevenDaysOrdersCountView(ModelViewSet):
+#     serializer_class = PreviousSevenDaysOrdersCountSerializer
+#     queryset = OrdersHeader.objects.all()[:1]
+
+class PreviousSevenDaysOrdersCountView(APIView):
+    def get(self, request):
+        qs = OrdersHeader.objects.all().first()
+        serializer = PreviousSevenDaysOrdersCountSerializer(instance=qs)
+        return Response(serializer.data)
 
 
-class PreviousSevenDaysSalesCountView(ModelViewSet):
-    serializer_class = PreviousSevenDaysSalesCountSerializer
-    queryset = OrdersHeader.objects.all()[:1]
+# class PreviousSevenDaysSalesCountView(ModelViewSet):
+#     serializer_class = PreviousSevenDaysSalesCountSerializer
+#     queryset = OrdersHeader.objects.all()[:1]
+
+class PreviousSevenDaysSalesCountView(APIView):
+    def get(self, request):
+        qs = OrdersHeader.objects.all().first()
+        serializer = PreviousSevenDaysSalesCountSerializer(instance=qs)
+        return Response(serializer.data)
 
 
-class PreviousHalfYearsMonthsRevenueView(ModelViewSet):
-    serializer_class = PreviousHalfYearsMonthsRevenueSerializer
-    queryset = OrdersHeader.objects.all()[:1]
+# class PreviousHalfYearsMonthsRevenueView(ModelViewSet):
+#     serializer_class = PreviousHalfYearsMonthsRevenueSerializer
+#     queryset = OrdersHeader.objects.all()[:1]
+
+class PreviousHalfYearsMonthsRevenueView(APIView):
+    def get(self, request):
+        qs = OrdersHeader.objects.all().first()
+        serializer = PreviousHalfYearsMonthsRevenueSerializer(instance=qs)
+        return Response(serializer.data)
 
 
-class TopTenCustomersView(ModelViewSet):
-    serializer_class = TopTenCustomersSerializer
-    queryset = OrdersHeader.objects.all()[:1]
+# class TopTenCustomersView(ModelViewSet):
+#     serializer_class = TopTenCustomersSerializer
+#     queryset = OrdersHeader.objects.all()[:1]
+
+class TopTenCustomersView(APIView):
+    def get(self, request):
+        qs = OrdersHeader.objects.all().first()
+        serializer = TopTenCustomersSerializer(instance=qs)
+        return Response(serializer.data)
